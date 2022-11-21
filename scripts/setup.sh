4019d58e5a64c4196490d0ee2821c5c0d43abb92
michele_launi <michele.launi@quantyca.it>
2022-11-21 12:15:09 +0100

    Setup script

---
+++ scripts/setup.sh
@@ -0,0 +1,13 @@
+
+#mkdocs-material+dependencies
+pip install -U mkdocs-material
+
+#Git Dates
+pip install -U mkdocs-git-revision-date-localized-plugin
+
+#Social Cards (Ubuntu)
+pip install -U pillow cairosvg
+apt-get install libcairo2-dev libfreetype6-dev libffi-dev libjpeg-dev libpng-dev libz-dev
+
+#JSON Schema for Humans
+pip install -U json-schema-for-humans
